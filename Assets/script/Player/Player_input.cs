c548db02abff774e650086897a44220eaf55176b
yjpuuynno <96729294+yjpuuynno@users.noreply.github.com>
2022-06-10 22:50:59 +0900

    Walk함수 추가
    AddForce로 가속한다.
    목표 속도에 도달하면 가속하지 않는다

--- Assets/script/Player/Player_input.cs
+++ Assets/script/Player/Player_input.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Player_input : MonoBehaviour
 {
+    public float x;
+    public float y;
+    public float xRaw;
+    public float yRaw;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,9 @@ public class Player_input : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        xRaw = Input.GetAxisRaw("Horizontal");
+        yRaw = Input.GetAxisRaw("Vertical");
     }
 }
--- Assets/script/Player/Player_movement.cs
+++ Assets/script/Player/Player_movement.cs
@@ -4,26 +4,37 @@ using UnityEngine;
 
 public class Player_movement : MonoBehaviour
 {
+    [HideInInspector]
     public Rigidbody2D rigidbody2D;
-    private Player_collider player_Collider;
-    private Player_input player_Input;
+    Player_collider pCollider;
+    Player_input pInput;
+
+    [Space]
+    [Header("Input")]
+    private float moveInput;
+    [Space]
+    [Header("Stats")]
+    public float moveSpeed = 10;
+    public float acceleration = 5;
+    public float decceleration = 5;
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
-        player_Collider = GetComponent<Player_collider>();
-        player_Input = GetComponent<Player_input>();
+        pCollider = GetComponent<Player_collider>();
+        pInput = GetComponent<Player_input>();
+        rigidbody2D.constraints = RigidbodyConstraints2D.FreezeRotation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = pInput.xRaw;
     }
 
     void FixedUpdate()
     {
-        
+        Walk();
     }
 
     void Walk()//걷는다
@@ -32,6 +43,13 @@ public class Player_movement : MonoBehaviour
         현제속력+=가속도;
         }
         */
+        float velPower = 1f;
+        float pSpeed = moveInput * moveSpeed;
+        float speedDif = pSpeed - rigidbody2D.velocity.x;
+        float accelRate = (Mathf.Abs(pSpeed)>0.01f) ? acceleration : decceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDif)*accelRate , velPower) *Mathf.Sign(speedDif);
+
+        rigidbody2D.AddForce(movement * Vector2.right);
     }
     void Jump()//점프한다
     {/*
